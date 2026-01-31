10e78b9e3c7702fe69d64010d3b047d6378ea243
kkyunga <rlaruddk1020@naver.com>
2026-01-31 23:26:15 +0900

    Initial commit

---
+++ src/main/java/org/back/devsnackshop_back/DevSnackshopBackApplication.java
@@ -0,0 +1,13 @@
+package org.back.devsnackshop_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSnackshopBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevSnackshopBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/back/devsnackshop_back/DevSnackshopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.back.devsnackshop_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSnackshopBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
